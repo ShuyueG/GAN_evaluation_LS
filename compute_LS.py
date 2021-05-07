ec01e6861f1d6b0e605a3f88b25cc9bc1ea55372
Shuyue (Frank) Guan <frankshuyueguan@gwu.edu>
2021-05-07 01:20:41 -0400

    Create compute_LS.py

---
+++ compute_LS.py
@@ -0,0 +1,14 @@
+'''
+This is an example of computing the Likeness Score: LS on datasets of real images and generated images by GANs.
+It includes codes to compute the *2-class* distance-based separability index (DSI). There are two versions (CPU and GPU) of DSI.
+
+Inputs:           Two folders have real images and generated images
+
+Related paper:      A Novel Measure to Evaluate Generative Adversarial Networks Based on Direct Analysis of Generated Images
+                    [In press] Neural Computing and Applications, 2021
+                    https://arxiv.org/abs/2002.12345
+                    
+By:                 Shuyue Guan
+                    https://shuyueg.github.io/
+'''
+
